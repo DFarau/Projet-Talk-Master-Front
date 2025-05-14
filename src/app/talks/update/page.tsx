842cfa22e88907143316674dbd34e1b489aa3bbd
Charles Lambret <charleslambretpro@gmail.com>
2025-05-14 14:08:56 +0200

    feat: talk form UI

---
+++ src/app/talks/update/page.tsx
@@ -0,0 +1,9 @@
+import TalkForm from "@/components/TalkForm";
+
+export default function TalkPage() {
+  return (
+    <div className="flex flex-col w-full items-center justify-center min-h-screen bg-white">
+      <TalkForm type="update" />
+    </div>
+  );
+}
---
+++ src/components/TalkForm.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import React, { useState } from "react";
+
+
+interface TalkFormProps {
+  type: "create" | "update" ;
+  
+}
+
+const TalkForm: React.FC<TalkFormProps> = ({ type }) => {
+  const isCreate = type === "create";
+  const isUpdate = type === "update";
+
+  return (
+    <div className="flex h-screen bg-white w-full justify-center items-center">
+      <div className="flex items-center justify-center w-1/3">
+        <div className="bg-white shadow-md border rounded-lg p-8 w-full max-w-md ">
+          <h1 className="font-medium text-[25px]  mb-5">{isCreate ? "Proposez un nouveau talk" : "Mettez à jour votre talk"}</h1>
+          
+          <form className="space-y-4">
+            <div className="flex flex-col gap-3">
+              <label htmlFor="title" className="block text-[16px] font-normal w-full ">
+                Titre du talk
+              </label>
+              <input
+                type="text"
+                id="title"
+                className="mt-1 mb-7 block w-full h-12 px-4 rounded-md border border-black focus:border-blue-600 focus:ring-blue-600 text-[14px]"
+                placeholder="Renseignez ici le titre du talk"
+              />
+              <div className="flex w-full flex-row items-center justify-between gap-5">
+                  <div className="flex flex-col w-1/2">
+                    <label htmlFor="level" className="block text-[16px] font-normal">
+                      Niveau de difficulté
+                    </label>
+                    <select
+                      id="level"
+                      className="mt-1 mb-7 block w-full h-12 px-4 rounded-md border border-black focus:border-blue-600 focus:ring-blue-600 text-[14px]"
+                    >
+                      <option value="beginner">Débutant</option>
+                      <option value="intermediate">Intermédiaire</option>
+                      <option value="advanced">Avancé</option>
+                    </select>
+                  </div>
+                  
+                  <div className="flex flex-col w-1/2">
+                    <label htmlFor="date" className="block text-[16px] font-normal">
+                      Date du talk
+                    </label>
+                    <input
+                      type="date"
+                      id="date"
+                      className="mt-1 mb-7 block w-full h-12 px-4 rounded-md border border-black focus:border-blue-600 focus:ring-blue-600 text-[14px]"
+                      />
+                  </div>
+              </div>
+              
+              <div className="flex w-full flex-row items-center justify-between gap-5">
+                <div className="flex flex-col w-1/2 ">
+                  <label htmlFor="start" className="text-[16px] font-normal">
+                  Horaire de début
+                  </label>
+                  <input 
+                  type="time"
+                  id="start"
+                  min="09:00"
+                  max="19:00"
+                  className="w-full p-4 rounded-md border border-black focus:border-blue-600 focus:ring-blue-600 text-[14px]"
+                  />
+                </div>
+                <div className="flex flex-col w-1/2 ">
+                <label htmlFor="end" className="text-[16px] font-normal">
+                    Horaire de fin
+                  </label>
+                  <input
+                    type ="time"
+                    id="end"
+                    className="w-full p-4 rounded-md border border-black focus:border-blue-600 focus:ring-blue-600 text-[14px]"
+                  />
+                </div>
+                  
+              </div>
+              <label htmlFor="description" className="block text-[16px] font-normal">
+                Description
+              </label>
+              <textarea
+                id="description"
+                className="mt-1 mb-7 block w-full h-32 px-4 rounded-md border border-black focus:border-blue-600 focus:ring-blue-600 text-[14px]"
+                placeholder="Renseignez ici la description du talk"
+              />
+              
+            </div>
+            
+            <button
+              type="submit"
+              className="w-full h-12 bg-blue-800 hover:bg-green-500 hover:cursor-pointer  text-[16px] font-medium text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2"
+            >
+              {isCreate
+              ? "Proposer le talk"
+              : "Mettre à jour le talk"}
+
+            </button>
+          </form>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TalkForm;
